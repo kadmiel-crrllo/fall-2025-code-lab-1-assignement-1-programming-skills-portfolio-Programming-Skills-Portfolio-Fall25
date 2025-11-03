ea4d3d7f646f86c3e8a4e5b5f21e9a87a77ab982
kadmiel-crrllo <kjadmiel@gmail.com>
2025-11-04 03:08:40 +0400

    Add files via upload

---
+++ Exercise 08 - Simple Search/07-SomeCounting.py
@@ -0,0 +1,14 @@
+for num in range(51):
+    print(num)
+
+for number in range(50, -1):
+    print(number)
+
+for count in range(30, 51):
+    print(count)
+
+for value in range(50, -2):
+    print(value)
+
+for item in range(100, 201, 5):
+    print(item)
