feb16ac1d8fe86eda3bbdf9e0827555f007d4f3b
kadmiel-crrllo <kjadmiel@gmail.com>
2025-11-12 21:13:07 +0400

    completed exercise 7

---
+++ Exercise 07 - Some Counting/07-SomeCounting.py
@@ -0,0 +1,19 @@
+# print numbers from 0 to 50
+for num in range(51):
+    print(num)
+
+# print numbers from 50 down to 0
+for number in range(50, -1, -1):
+    print(number)
+
+# print numbers from 30 up to 50
+for count in range(30, 51):
+    print(count)
+
+# print numbers from 50 down to -1
+for value in range(50, -2, -1):
+    print(value)
+
+# print numbers from 100 to 200, counting by 5
+for item in range(100, 201, 5):
+    print(item)
