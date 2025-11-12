28749ebfaac950871cfd2bbe106d6c5dd33f9d6a
kadmiel-crrllo <kjadmiel@gmail.com>
2025-11-12 21:13:30 +0400

    Delete Exercise 08 - Simple Search/08-SimpleSearch.py

--- Exercise 08 - Simple Search/08-SimpleSearch.py
+++
@@ -1,9 +0,0 @@
-names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
-
-search_name = input("Input the name of who you are looking for:")
-
-if search_name in names:
-    seat = names.index(search_name) + 1
-    print(f"Found {search_name}! they are at seat {seat}.")
-else:
-    print(f"Sorry, {search_name} is not in the list.")
