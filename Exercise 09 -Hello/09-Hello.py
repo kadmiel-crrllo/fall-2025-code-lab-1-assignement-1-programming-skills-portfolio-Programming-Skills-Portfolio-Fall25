09029bd28552f573ecb3b153b981438a36e1b9c0
kadmiel-crrllo <kjadmiel@gmail.com>
2025-11-12 21:14:19 +0400

    completed exercise 9

---
+++ Exercise 09 -Hello/09-Hello.py
@@ -0,0 +1,11 @@
+# define a function that says Hello
+def hello():
+    print("Hello")
+
+# define the main function that runs hello()
+def main():
+    hello()
+
+# run main() only if this file is being run directly
+if __name__ == "__main__":
+    main()
